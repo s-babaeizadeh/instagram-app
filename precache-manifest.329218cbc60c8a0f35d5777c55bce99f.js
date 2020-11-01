fdb34c01455407e1252c08844456a78b7c657e59
SoudehBabaeizadeh <babaeizadeh.s@gmail.com>
2020-11-01 12:49:54 +0330

    Updates

---
+++ precache-manifest.329218cbc60c8a0f35d5777c55bce99f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "627c91b05c4fdeebe52fc910e00c4f73",
+    "url": "/instagram-app/index.html"
+  },
+  {
+    "revision": "ca66e2e0b2e1b8a639b7",
+    "url": "/instagram-app/static/css/main.2c8dfe01.chunk.css"
+  },
+  {
+    "revision": "a4e8721a15b577d8fd46",
+    "url": "/instagram-app/static/js/2.dcb19c00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/instagram-app/static/js/2.dcb19c00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca66e2e0b2e1b8a639b7",
+    "url": "/instagram-app/static/js/main.3b14a517.chunk.js"
+  },
+  {
+    "revision": "27abe1b62cdee3032149",
+    "url": "/instagram-app/static/js/runtime-main.571e1984.js"
+  },
+  {
+    "revision": "ba487a8b6dc9f28dba6ec6374a3f1fc0",
+    "url": "/instagram-app/static/media/bg7.ba487a8b.jpg"
+  }
+]);
